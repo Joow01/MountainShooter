6f28e83606576306fef4bbd11588a39af2b0807f
Jonatas <jonatas240407@gmail.com>
2026-06-18 19:23:33 -0300

    Some changes in BG

--- Code/const.py
+++ Code/const.py
@@ -1,4 +1,7 @@
 from pygame.examples.grid import WINDOW_HEIGHT
+#C
+COLOR_BLOOD = (90,0,0)
 
+#W
 WIN_WINDTH = 1672
 WINDOW_HEIGHT = 941
--- Code/menu.py
+++ Code/menu.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
-from pygame import Surface, Rect
-from pygame.font import Font
+from Code.const import WIN_WINDTH, COLOR_BLOOD
 
 
 class Menu :
@@ -10,12 +9,16 @@ class Menu :
         self.window = window
         self.surf = pygame.image.load('./assets/MenuBG.png')
         self.rect = self.surf.get_rect(left=0, top=0)
+        self.title_font = pygame.font.Font(
+            './assets/Fonte/static/Cinzel-Bold.ttf',50 )
 
     def run(self, ):
         pygame.mixer_music.load('./assets/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=100, text="Sangue", text_color=COLOR_BLOOD, text_center_pos=(WIN_WINDTH / 2, 70))
+            self.menu_text(text_size=100, text="Eterno", text_color=COLOR_BLOOD, text_center_pos=(WIN_WINDTH / 2, 145))
             pygame.display.flip()
 
             # Check for all events
@@ -24,8 +27,22 @@ class Menu :
                     pygame.quit() # Close Window
                     quit() # End Pygame
 
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
-        self.window.blit(source=text_surf, dest=text_rect)
+    def menu_text(self, text_size: int, text: str,
+                  text_color: tuple, text_center_pos: tuple):
+
+        text_font = pygame.font.Font(
+            "./assets/Fonte/static/Cinzel-Bold.ttf",
+            text_size
+        )
+
+        # Sombra preta
+        shadow_surf = text_font.render(text, True, (0, 0, 0))
+        shadow_rect = shadow_surf.get_rect(
+            center=(text_center_pos[0] + 3, text_center_pos[1] + 3)
+        )
+
+        #Texto principal
+        text_surf = text_font.render(text, True, text_color)
+        text_rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(shadow_surf, shadow_rect)
+        self.window.blit(text_surf, text_rect)
