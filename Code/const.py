f49747b28a9837eaa4e56083994b2caf1db6a557
Jonatas <jonatas240407@gmail.com>
2026-06-18 16:34:00 -0300

    Menu text and music

---
+++ Code/const.py
@@ -0,0 +1,4 @@
+from pygame.examples.grid import WINDOW_HEIGHT
+
+WIN_WINDTH = 1672
+WINDOW_HEIGHT = 941
--- Code/game.py
+++ Code/game.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from Code.const import WIN_WINDTH, WINDOW_HEIGHT
 from Code.menu import Menu
 
 class Game:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 600))
+        self.window = pygame.display.set_mode(size=(WIN_WINDTH, WINDOW_HEIGHT))
 
     def run(self, ):
         while True:
@@ -16,8 +17,3 @@ class Game:
             menu.run()
             pass
 
-            # Check for all events
-            # for event in pygame.event.get():
-            #   if event.type == pygame.QUIT:
-            #        pygame.quit() # Close Window
-            #       quit() # End Pygame
